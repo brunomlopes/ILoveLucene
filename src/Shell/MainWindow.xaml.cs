a8ad2695f12a7df0355a169b0cc44fc08533529f
brunomlopes <brunomlopes@gmail.com>
2010-11-14 01:14:54 +0000

    Window now toggles

--- src/Shell/MainWindow.xaml.cs
+++ src/Shell/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ILoveLucene.WindowsInterop;
 
 namespace ILoveLucene
 {
@@ -19,9 +20,45 @@ namespace ILoveLucene
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly KeyboardHandler _globalHotKeyHandler;
+        private readonly FocusHandler _focusHandler;
+
         public MainWindow()
         {
             InitializeComponent();
+            _globalHotKeyHandler = new KeyboardHandler(this, Toggle);
+            _focusHandler = new FocusHandler(this);
+            Input.Focus();
+            Input.Text = "Start typing...";
+            Input.SelectAll();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            if(_globalHotKeyHandler != null)
+                _globalHotKeyHandler.Dispose(); 
+        }
+
+        public void Toggle()
+        {
+            if (this.Visibility == Visibility.Hidden)
+            {
+                this.Visibility = Visibility.Visible;
+                this.Show();
+                _focusHandler.SetForegroundWindow();
+                SetTextTo(string.Empty);
+                Input.Focus();
+                
+            }
+            else
+            {
+                this.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void SetTextTo(string text)
+        {
+            Input.Text = text;
         }
     }
 }
---
+++ src/Shell/WindowsInterop/FocusHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace ILoveLucene.WindowsInterop
+{
+    public class FocusHandler
+    {
+        private readonly Window _window;
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+        WindowInteropHelper _host;
+
+        public FocusHandler(Window window)
+        {
+            _window = window;
+            _host = new WindowInteropHelper(window);
+
+        }
+        public void SetForegroundWindow()
+        {
+            SetForegroundWindow(_host.Handle);
+        }
+    }
+}
