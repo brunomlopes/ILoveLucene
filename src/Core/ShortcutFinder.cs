6e32eb611c9f7180f7ab3d79ce61740435a92edd
brunomlopes <brunomlopes@gmail.com>
2010-11-15 03:44:11 +0000

    Better multithreaded version.

--- src/Core/ShortcutFinder.cs
+++ src/Core/ShortcutFinder.cs
@@ -13,7 +13,15 @@ namespace Core
         private HashSet<FileInfo> _shortcutPaths;
         public HashSet<FileInfo> ShortcutPaths
         {
-            get { return _shortcutPaths; }
+            get
+            {
+                HashSet<FileInfo> shortcuts;
+                lock (_shortcutPathsLock)
+                {
+                    shortcuts = new HashSet<FileInfo>(_shortcutPaths);
+                }
+                return shortcuts;
+            }
         }
         private static object _shortcutPathsLock = new object();
 
@@ -40,44 +48,41 @@ namespace Core
         private void ScanDirectoryForShortcuts(string s, List<string> extensions)
         {
             var currentDir = new DirectoryInfo(s);
+
             Task.Factory.StartNew(() =>
-                                  currentDir
-                                      .GetFiles("*.*", SearchOption.TopDirectoryOnly)
-                                      .AsParallel()
-                                      .ForAll(f =>
-                                                  {
-                                                      try
-                                                      {
-                                                          if (extensions.Contains(f.Extension))
-                                                          {
-                                                              Debug.WriteLine("Found " + f.FullName);
-                                                              lock (_shortcutPathsLock)
-                                                              {
-                                                                  _shortcutPaths.Add(f);
-                                                              }
-                                                          }
-                                                      }
-                                                      catch (UnauthorizedAccessException e)
-                                                      {
-                                                          // Oh well, no biggie I guess
-                                                      }
-                                                  }))
+                                      {
+                                          try
+                                          {
+                                              var fileInfos = currentDir.GetFiles("*.*", SearchOption.TopDirectoryOnly)
+                                                  .Where(f => extensions.Contains(f.Extension)).ToList();
+                                              Debug.WriteLine("Found {0} files in {1}", fileInfos.Count(), currentDir);
+                                              lock (_shortcutPathsLock)
+                                              {
+                                                  _shortcutPaths.UnionWith(fileInfos);
+                                              }
+                                          }
+                                          catch (UnauthorizedAccessException e)
+                                          {
+                                              // Oh well, no biggie I guess
+                                          }
+                                      })
                 .GuardForException(e => Debug.WriteLine("Exception on task:" + e));
+           
             Task.Factory.StartNew(() =>
-                                  currentDir
-                                      .GetDirectories()
-                                      .AsParallel()
-                                      .ForAll(d =>
-                                                  {
-                                                      try
-                                                      {
-                                                          ScanDirectoryForShortcuts(d.FullName, extensions);
-                                                      }
-                                                      catch (UnauthorizedAccessException e)
-                                                      {
-                                                          // Oh well, no biggie I guess
-                                                      }
-                                                  }))
+                                      {
+                                          DirectoryInfo[] directoryInfos;
+                                          try
+                                          {
+                                              directoryInfos = currentDir.GetDirectories();
+                                          }
+                                          catch(UnauthorizedAccessException e)
+                                          {
+                                              return;
+                                          }
+                                          directoryInfos
+                                              .AsParallel()
+                                              .ForAll(d => ScanDirectoryForShortcuts(d.FullName, extensions));
+                                      })
                 .GuardForException(e => Debug.WriteLine("Exception on task:" + e));
         }
     }
