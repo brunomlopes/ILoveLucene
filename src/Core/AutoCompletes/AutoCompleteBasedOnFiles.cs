2300b6ac4d2ac3967084b5c8a3f13ef73a7e9863
brunomlopes <brunomlopes@gmail.com>
2010-11-15 03:52:48 +0000

    Moved files around

---
+++ src/Core/AutoCompletes/AutoCompleteBasedOnFiles.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using Core.Abstractions;
+
+namespace Core.AutoCompletes
+{
+    [Export(typeof(IAutoCompleteText))]
+    public class AutoCompleteBasedOnFiles : IAutoCompleteText
+    {
+        private class Command : ICommand
+        {
+            private readonly FileInfo _shortcut;
+
+            public Command(FileInfo shortcut)
+            {
+                _shortcut = shortcut;
+            }
+
+            public string Text { get { return _shortcut.Name; } }
+            public string Description { get { return _shortcut.FullName; } }
+
+            public void Execute()
+            {
+                System.Diagnostics.Process.Start(_shortcut.FullName);
+            }
+        }
+
+        private ShortcutFinder _shortcutFinder;
+
+        public AutoCompleteBasedOnFiles()
+        {
+            _shortcutFinder = new ShortcutFinder();
+        }
+
+        public AutoCompletionResult Autocomplete(string text)
+        {
+            var shortcuts =
+                _shortcutFinder.ShortcutPaths
+                    .Where(fileInfo => fileInfo.Name.StartsWith(text, StringComparison.CurrentCultureIgnoreCase))
+                    .Take(10)
+                    .Select(s => new Command(s));
+            return AutoCompletionResult.OrderedResult(text, shortcuts);
+        }
+    }
+}
