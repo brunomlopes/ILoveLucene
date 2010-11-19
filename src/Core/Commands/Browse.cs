004993491e3a50b0c9a3f685de2e23c368caca23
brunomlopes <brunomlopes@gmail.com>
2010-11-19 15:30:48 +0000

    Browse command

---
+++ src/Core/Commands/Browse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using Core.Abstractions;
+
+namespace Core.Commands
+{
+    [Export(typeof(ICommand))]
+    public class Browse : ICommandWithArguments
+    {
+        public string Text
+        {
+            get { return "Browse"; }
+        }
+
+        public string Description
+        {
+            get { return "Launch a browser"; }
+        }
+
+        public void Execute()
+        {
+            // NO-OP
+        }
+
+        public void Execute(string arguments)
+        {
+            Uri uri;
+            if (Uri.TryCreate(arguments, UriKind.Absolute, out uri))
+            {
+                Process.Start(uri.ToString());
+            }
+            else
+            {
+                throw new InvalidOperationException("'{0}' doesn't seem like an url");
+            }
+        }
+    }
+}
