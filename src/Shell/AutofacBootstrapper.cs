57739ed350a1312f6e3972c7d327ef7c8ed20dab
brunomlopes <brunomlopes@gmail.com>
2010-11-21 00:04:37 +0000

    Cleanup before merge

--- src/Shell/AutofacBootstrapper.cs
+++ src/Shell/AutofacBootstrapper.cs
@@ -1,15 +1,12 @@
-using System.Collections.Generic;
 using System.ComponentModel.Composition.Hosting;
 using System.ComponentModel.Composition.Primitives;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using Autofac;
-using Autofac.Integration.Mef;
 using Caliburn.Micro;
 using Core.Abstractions;
 using Core.Lucene;
-using ILoveLucene.Commands;
 using ILoveLucene.Modules;
 using ILoveLucene.ViewModels;
 using System.ComponentModel.Composition;
@@ -47,27 +44,6 @@ namespace ILoveLucene
 
             builder.RegisterType<MainWindowViewModel>().As<IShell>();
             builder.RegisterType<AutoCompleteBasedOnLucene>().As<IAutoCompleteText>();
-            
-            //builder.RegisterAssemblyTypes(typeof(AutoCompleteBasedOnLucene).Assembly)
-            //    .Where(t => t.IsPublic && t.Namespace != typeof(Core.Abstractions.ICommand).Namespace)
-            //    .AsImplementedInterfaces()
-            //    .SingleInstance();
-
-            //builder.RegisterAssemblyTypes(this.GetType().Assembly)
-            //    .InNamespaceOf<ExitApplication>()
-            //    .AsImplementedInterfaces()
-            //    .SingleInstance();
-
-            //var batch = new CompositionBatch();
-
-            //batch.AddExportedValue<IWindowManager>(new WindowManager());
-            //batch.AddExportedValue<IEventAggregator>(new EventAggregator());
-            //batch.AddExportedValue(Container);
-        }
-
-        public interface Fake
-        {
-            
         }
     }
 }
