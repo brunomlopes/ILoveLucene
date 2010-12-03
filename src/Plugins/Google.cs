0385ea48e2f6f83b8b9d5880fec8818d29aac957
brunomlopes <brunomlopes@gmail.com>
2010-12-03 08:38:09 +0000

    Google plugin

---
+++ src/Plugins/Google.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using Core.Abstractions;
+
+namespace Plugins
+{
+    [Export(typeof (IActOnItem))]
+    public class Google : BaseActOnTypedItem<string>
+    {
+        public override void ActOn(ITypedItem<string> item)
+        {
+            var uri = new Uri("http://www.google.com/search?q=" + Uri.EscapeDataString(item.Item), UriKind.Absolute);
+            Process.Start(uri.ToString());
+        }
+
+        public override string Text
+        {
+            get { return "Google"; }
+        }
+    }
+}
