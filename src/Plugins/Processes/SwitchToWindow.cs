934031329feb60a4cc7638add80de84de0f6ff30
brunomlopes <brunomlopes@gmail.com>
2010-12-25 04:50:40 +0000

    Now with "switch to this window"

---
+++ src/Plugins/Processes/SwitchToWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Core.Abstractions;
+
+namespace Plugins.Processes
+{
+    [Export(typeof(IActOnItem))]
+    public class SwitchToWindow : BaseActOnTypedItem<Process>, ICanActOnTypedItem<Process>
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        static extern uint SwitchToThisWindow(IntPtr hwnd, bool isAltTab);
+
+        public override void ActOn(Process item)
+        {
+            SwitchToThisWindow(item.MainWindowHandle, true);
+        }
+
+        public bool CanActOn(Process item)
+        {
+            return item.MainWindowHandle != IntPtr.Zero;
+        }
+    }
+}
