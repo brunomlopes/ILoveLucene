e3eb6c0622f4550c65e0183b0a969f7b42ae5bc8
brunomlopes <brunomlopes@gmail.com>
2011-05-15 01:31:24 +0100

    Open in notepad2

---
+++ src/Plugins.WindowsEnvironment/OpenInNotepad2.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using Core.Abstractions;
+
+namespace Plugins.WindowsEnvironment
+{
+    [Export(typeof(IActOnItem))]
+    public class OpenInNotepad2 : BaseActOnTypedItem<FileInfo>
+    {
+        public override void ActOn(FileInfo item)
+        {
+            Process.Start("notepad2", item.FullName);
+        }
+    }
+}
