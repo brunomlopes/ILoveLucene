36b5b58d8d71d4cc6ac269001377722e4a6375f3
Bruno Lopes <brunomlopes@gmail.com>
2013-11-25 12:23:35 +0000

    Indents were incorret for python code

--- src/Plugins/IronPython/Sample.py
+++ src/Plugins/IronPython/Sample.py
@@ -1,23 +1,21 @@
-﻿import System
+import System
 clr.AddReference("System.Windows.Forms")
 
 @export(ICommand)
 class MustScream(ICommand):
-	@property
-	def Text(self):
-		return "MustScream"
+    @property
+    def Text(self):
+        return "MustScream"
 
-	@property
-	def Description(self):
-		return "I have no dialog box but I must scream"
+    @property
+    def Description(self):
+        return "I have no dialog box but I must scream"
 
-	def Act(self):
-		System.Windows.Forms.MessageBox.Show("Screaming", "Shout!", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning)
+    def Act(self):
+        System.Windows.Forms.MessageBox.Show("Screaming", "Shout!", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning)
 
 @export(IActOnItem)
 class ActOnString(BaseActOnTypedItem[str]):
     def ActOn(self, text):
         System.Windows.Forms.MessageBox.Show(text, "Shout!", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning)
 
-#def MustScreamFunction(args):
-#	System.Windows.Forms.MessageBox.Show(args, "Shout the function!", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning)
