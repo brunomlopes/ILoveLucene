b6d26ea07889bc2ea5d248eac1b0d7630f92bbfc
Bruno Lopes <brunomlopes@gmail.com>
2015-05-21 19:59:14 +0100

    Item source for visual studio recent items

---
+++ src/Plugins/VisualStudio/VisualStudioRecentItemsSource.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using Core.API;
+using Microsoft.Win32;
+
+namespace Plugins.VisualStudio
+{
+    [Export(typeof(IItemSource))]
+    public class VisualStudioRecentItemsSource : BaseItemSource
+    {
+        public override IEnumerable<object> GetItems()
+        {
+            var versionsKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\VisualStudio\");
+
+            foreach (var subKeyName in versionsKey?.GetSubKeyNames() ?? new string[] { })
+            {
+                var subKey = versionsKey.OpenSubKey(subKeyName);
+                var projectMruKey = subKey?.OpenSubKey("ProjectMRUList");
+
+                foreach (var projectKey in projectMruKey?.GetValueNames() ?? new string[] { })
+                {
+                    var mruInfo = projectMruKey.GetValue(projectKey).ToString();
+                    var path = mruInfo.Split('|')[0];
+                    var fileInfo = new FileInfo(path);
+                    if (fileInfo.Exists) yield return fileInfo;
+                }
+            }
+        }
+    }
+}
